3903e4843eb9f4a30ace6203da73420035e65c9e
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-04-21 16:47:22 +0200

    install vue cli project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,6 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-import App from "./App.vue";
-import router from "./router";
-
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+createApp(App).use(store).use(router).mount('#app')
