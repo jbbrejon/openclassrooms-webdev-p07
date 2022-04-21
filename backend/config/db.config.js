668a2291d0990249f73941fc9a7b7ba7624a0fe3
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-04-21 09:57:33 +0200

    authentication information moved to .env file

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,8 +1,13 @@
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "ocSqlP7groupomni4",
-    DB: "oc_groupomania",
+    HOST: dbHost,
+    USER: dbUser,
+    PASSWORD: dbPassword,
+    DB: dbName,
     dialect: "mysql",
     pool: {
         max: 5,
